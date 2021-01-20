d42f1dab62ed97db8372a9932558cefcd8c933d7
tellora <rtellodefariapaulin@ext.ec.europa.eu>
2021-01-20 16:40:02 +0000

    Initial commit

---
+++ scripts/testing_oja.R
@@ -0,0 +1,17 @@
+library(wihoja)
+
+
+oja <- query_athena(" select * from estat_dsl2531b_oja.ft_document_en_V8 limit 10")
+
+company_name_lu <- query_athena(" select companyname from estat_dsl2531b_oja.ft_document_en_V8 where idcountry='LU' limit 1000")
+
+
+oja <- get_oja(idcountry = 'LU', lang = 'pt')
+
+
+oja_count <- get_oja_count(idcountry = 'LU', lang = 'pt', group_by = 'sourcecountry')
+
+
+noja <- get_oja_count(group_by = "source")
+
+nojv <- get_ojv_count(group_by = "source")
