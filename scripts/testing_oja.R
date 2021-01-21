0afb8f2e19357045aa5de9afa16dade3a66d05b8
Raquel Paulino <raquel.tello-de-faria-paulino@ext.ec.europa.eu>
2021-01-21 16:46:54 +0000

    Changed to V8

--- scripts/testing_oja.R
+++ scripts/testing_oja.R
@@ -1,12 +1,17 @@
+
+
+install.packages("devtools")
+library(devtools)
+install_github("https://github.com/eurostat/wihoja.git", auth_token="9f8536122e521fe1a7083f23913d501055827994")
+
 library(wihoja)
-
-
-oja <- query_athena(" select * from estat_dsl2531b_oja.ft_document_en_V5 limit 10")
+oja <- query_athena(" select * from ")
+oja <- query_athena(" select * from estat_dsl2531b_oja.ft_document_en_V8 limit 10")
 
 company_name_lu <- query_athena(" select companyname from estat_dsl2531b_oja.ft_document_en_V8 where idcountry='LU' limit 1000")
 
 
-oja <- get_oja(idcountry = 'LU', lang = 'pt')
+oja <- get_oja(idcountry = 'PT', lang = 'en')
 
 
 oja_count <- get_oja_count(idcountry = 'LU', lang = 'pt', group_by = 'sourcecountry')
